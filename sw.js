c960786ed3354430c28c551efb386cc87f463cdc
hamuchira <okita.dqw@gmail.com>
2025-11-16 18:48:45 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Workerのバージョン - 自動的に日時で管理（編集不要！）
-const CACHE_VERSION = 'v-' + new Date().getTime();
+// Service Workerのバージョン - 更新時は必ずこの数字を変更してください！
+const CACHE_VERSION = 'v20241115-003';
 const CACHE_NAME = 'hamuchira-cache-' + CACHE_VERSION;
 
 // キャッシュするファイルのリスト
